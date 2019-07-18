774584f349be001027c3bbce2e7f381ecff6455b
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2019-07-19 01:24:25 +0200

    Put most of the module-dependent stuff for qxqueue.cpp into qxmodules.h.

--- src/cpp/qxqueue.cpp
+++ src/cpp/qxqueue.cpp
@@ -3,37 +3,35 @@
 #include <sstream>
 
 #include <boost/preprocessor/cat.hpp>
+#include <boost/preprocessor/facilities/empty.hpp>
+#include <boost/preprocessor/iteration/iterate.hpp>
 #include <boost/preprocessor/seq/elem.hpp>
 #include <boost/preprocessor/seq/for_each.hpp>
 #include <boost/preprocessor/seq/for_each_i.hpp>
-#include <boost/preprocessor/facilities/empty.hpp>
+#include <boost/preprocessor/stringize.hpp>
 
 #include "qxqueue.h"
 
-#include "preproc_Lexer"
-#include <quex/code_base/buffer/lexatoms/converter/iconv/Converter_IConv>
-#include <quex/code_base/buffer/lexatoms/converter/iconv/Converter_IConv.i>
-#include "hyphen_Lexer"
-#include <quex/code_base/buffer/lexatoms/converter/iconv/Converter_IConv>
-#include <quex/code_base/buffer/lexatoms/converter/iconv/Converter_IConv.i>
-#include "snt_Lexer"
-#include <quex/code_base/buffer/lexatoms/converter/iconv/Converter_IConv>
-#include <quex/code_base/buffer/lexatoms/converter/iconv/Converter_IConv.i>
-#include "sntcorr_Lexer"
-#include <quex/code_base/buffer/lexatoms/converter/iconv/Converter_IConv>
-#include <quex/code_base/buffer/lexatoms/converter/iconv/Converter_IConv.i>
-#include "token_Lexer"
-#include <quex/code_base/buffer/lexatoms/converter/iconv/Converter_IConv>
-#include <quex/code_base/buffer/lexatoms/converter/iconv/Converter_IConv.i>
-#include "convxml_Lexer"
-#include <quex/code_base/buffer/lexatoms/converter/iconv/Converter_IConv>
-#include <quex/code_base/buffer/lexatoms/converter/iconv/Converter_IConv.i>
-#include "convjson_Lexer"
-#include <quex/code_base/buffer/lexatoms/converter/iconv/Converter_IConv>
-#include <quex/code_base/buffer/lexatoms/converter/iconv/Converter_IConv.i>
-#include "convtsv_Lexer"
-#include <quex/code_base/buffer/lexatoms/converter/iconv/Converter_IConv>
-#include <quex/code_base/buffer/lexatoms/converter/iconv/Converter_IConv.i>
+/************* Macro stuff to generate lexer includes / namespaces ***********/
+
+// List of the modules for boost preprocessor to work with
+#define QX_MODULES (preproc) (hyphen) (snt) (sntcorr) (token) \
+                   (convxml) (convjson) (convtsv)
+// ... and the uppercase variants -- the PP cannot do capitalization 
+#define QX_MODULES_UPPER (PREPROC) (HYPHEN) (SNT) (SNTCORR) (TOKEN) \
+                         (CONVXML) (CONVJSON) (CONVTSV)
+
+#define QX_MODULE_CASE(r, data, index, elem) \
+            case BOOST_PP_SEQ_ELEM(index, QX_MODULES_UPPER): { \
+                BOOST_PP_CAT(elem, BOOST_PP_EMPTY())::module(istreams[i], ostreams[i]); \
+                break; \
+            }
+
+#define BOOST_PP_ITERATION_PARAMS_1 (3, (0, 7, "qxmodules.h"))
+
+#include BOOST_PP_ITERATE()
+
+/***************************** End of macro stuff ***************************/
 
 #include <quex/code_base/multi.i>
 #include <quex/code_base/definitions>
@@ -81,44 +79,6 @@ void QxQueue::clear_streams() {
     }
 }
 
-/****************** Macro stuff ****************/
-
-// List of the modules for boost preprocessor to work with
-#define MODULES (preproc) (hyphen) (snt) (sntcorr) (token) \
-                (convxml) (convjson) (convtsv)
-
-#define MODULES_UPPER (PREPROC) (HYPHEN) (SNT) (SNTCORR) (TOKEN) \
-                      (CONVXML) (CONVJSON) (CONVTSV)
-
-#define MODULE_NAMESPACE(r, data, elem) \
-namespace elem { \
-void module(std::istream* inp, std::ostream* out) { \
-    Lexer_ByteLoader* loader = Lexer_ByteLoader_stream_new(inp); \
-    Lexer_Converter* converter = Lexer_Converter_IConv_new("UTF8", nullptr); \
-    Lexer* lexer = Lexer::from_ByteLoader(loader, converter); \
-\
-    typename Lexer::token_type* token_p = 0x0; \
-    for (lexer->receive(&token_p); \
-         token_p->type_id() != BOOST_PP_CAT(elem, _TERMINATION); \
-         lexer->receive(&token_p)) { \
-        *out << QUEX_CONVERTER_STRING(unicode, char)(token_p->get_text()); \
-    } \
-    delete lexer; \
-} \
-}
-
-#define MODULE_CASE(r, data, index, elem) \
-            case BOOST_PP_SEQ_ELEM(index, MODULES_UPPER): { \
-                BOOST_PP_CAT(elem, BOOST_PP_EMPTY())::module(istreams[i], ostreams[i]); \
-                break; \
-            }
-
-/****************** End macro stuff ****************/
-
-
-BOOST_PP_SEQ_FOR_EACH(MODULE_NAMESPACE, _, MODULES)
-
-
 // public functions:
 void QxQueue::run(std::istream& inp, std::ostream& out /* =std::cout */) {
     clear_streams();
@@ -129,7 +89,7 @@ void QxQueue::run(std::istream& inp, std::ostream& out /* =std::cout */) {
 
     for (int i = 0; i < types.size(); ++i) {
         switch(types[i]) {
-            BOOST_PP_SEQ_FOR_EACH_I(MODULE_CASE, _, MODULES)
+            BOOST_PP_SEQ_FOR_EACH_I(QX_MODULE_CASE, _, QX_MODULES)
             default:
                 std::cerr << "Wrong module type!" << std::endl;
                 exit(1);
