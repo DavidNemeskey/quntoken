52ab014280baaab7ea24f4669162626ec941a5d7
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2019-07-19 16:11:48 +0200

    Get the C++ exceptions to the Python side. This is needed so that we can properly handle exceptions other than our own (e.g. the one thrown by the QunToken constructor).

--- src/cpp/quntoken_c.cpp
+++ src/cpp/quntoken_c.cpp
@@ -1,8 +1,16 @@
 #include <cstdbool>
+#include <string>
 #include "quntoken.h"
 
 /** Define C functions for the C++ class - as ctypes can only talk to C... */
 extern "C" {
+    /**
+     * A string storing the cause of the exceptions caught by the \c QunToken_
+     * functions. This errno-like functionality is needed as C doesn't handle
+     * exceptions, and hence ctypes doesn't either.
+     */
+    thread_local std::string foo;
+
     /**
      * Wraps the constructor.
      *
@@ -11,21 +19,39 @@ extern "C" {
      *          must happen on the Python side.
      */
     QunToken* QunToken_new(const char* format, const char* mode, bool hyphen) {
+        foo = "";
         try {
             return new QunToken(format, mode, hyphen);
         } catch (std::invalid_argument& ia) {
+            foo = ia.what();
             return nullptr;
         }
     }
+
     /** Wraps QunToken::tokenize(std::string). Not thread-safe. */
     const char* QunToken_tokenize(QunToken* qt, const char* input) {
+        foo = "";
         /*
          * Static, so that it does not go out of scope. There's the GIL,
          * so thread safety is not an issue anyway.
          */
         static std::string result;
         // = doesn't work, even though it probably should with move semantics?
-        result.assign(qt->tokenize(input));
+        try {
+            result.assign(qt->tokenize(input));
+        } catch (std::runtime_error& re) {
+            foo = re.what();
+            return nullptr;
+        }
         return result.c_str();
+    }
+
+    /**
+     * Returns the error message of the last exception caught by the other
+     * \c QunToken_ functions, if any. A successful call to those functions will
+     * erase the error message.
+     */
+    const char* QunToken_explain() {
+        return foo.c_str();
     }
 }
